fb0a395375ed36964fca51c7680a152507e12b05
Luiz Henrique <69431619+Hikari-no-Michi@users.noreply.github.com>
2025-03-27 18:47:06 -0300

    main

--- app/components/flashCard.tsx
+++ app/components/flashCard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
 type ConteudoDisciplina = {
   pergunta: string;
   resposta: string;
 };
+
 interface FlashcardProps {
   conteudoDisciplina: {
     pergunta: string;
@@ -33,7 +35,7 @@ const Flashcard: React.FC<FlashcardProps> = ({
     <div className="w-full max-w-lg mt-4 px-2 sm:px-0">
       <div
         onClick={() => setMostrarPergunta(!mostrarPergunta)}
-        className="bg-white p-2 rounded-lg shadow-md text-center sm:h-[300px] h-[150%]"
+        className="bg-white p-2 rounded-lg shadow-md text-center h-[55vh]"
       >
         {conteudoDisciplina ? (
           <>
@@ -42,51 +44,53 @@ const Flashcard: React.FC<FlashcardProps> = ({
                 {conteudoDisciplina.pergunta}
               </h2>
             ) : (
-              <p className="text-lg text-center text-gray-700 transition-opacity duration-500 opacity-100 flex items-center justify-center h-full px-4">
+              <p className="text-lg text-center text-gray-700 transition-opacity duration-500 opacity-100 flex items-center justify-center h-full px-4  overflow-y-auto">
                 {conteudoDisciplina.resposta}
               </p>
             )}
-
-            <div className="flex justify-center space-x-4 mt-4">
-              <div className="w-auto text-center">
-                <button className="text-red-500" 
-                onClick={(e) => {
-                  e.stopPropagation();
-                  selecionarProximoCard();
-                }}
-                >
-                  <FontAwesomeIcon icon={faTimesCircle} size="3x" />
-                </button>
-              </div>
-              <div className="w-auto text-center">
-                <button className="text-green-500"
-                onClick={(e) => {
-                  e.stopPropagation(); 
-                  selecionarProximoCard();
-                }}
-                
-                >
-                  <FontAwesomeIcon icon={faCheckCircle} size="3x" />
-                </button>
-              </div>
-            </div>
           </>
         ) : (
           <>
-            <h2 className="text-2xl font-bold text-[#0288d1] text-center mb-2 py-6 px-2 md:text-3xl md:px-3 lg:text-3xl lg:px-5 mt-3 lg:mt-9">
-              {mensagem}
-            </h2>
-            {!showDisciplinas && (
-              <button
-                className="bg-[#0288d1] text-white px-8 py-3 rounded-full shadow-lg border-2 border-[#0288d1] hover:bg-[#01579b] focus:outline-none"
-                onClick={() => setShowDisciplinas(true)}
-              >
-                Vamos Começar ?
-              </button>
-            )}
+            <div className="flex flex-col justify-center items-center h-full">
+              <h2 className="text-2xl font-bold text-[#0288d1] text-center md:text-3xl px-5 py-5">
+                {mensagem}
+              </h2>
+              {!showDisciplinas && (
+                <button
+                  className="bg-[#0288d1] text-white px-8 py-3 rounded-full shadow-lg border-2 border-[#0288d1] hover:bg-[#01579b] focus:outline-none"
+                  onClick={() => setShowDisciplinas(true)}
+                >
+                  Vamos Começar ?
+                </button>
+              )}
+            </div>
           </>
         )}
       </div>
+
+      {/* Só exibe os botões quando o conteúdo do card estiver disponível */}
+      {conteudoDisciplina && (
+        <div className="flex justify-center space-x-4 mt-4">
+          <div
+            className="w-[200px] text-center bg-slate-200 p-2 text-slate-700 select-none cursor-pointer"
+            onClick={(e) => {
+              e.stopPropagation();
+              selecionarProximoCard();
+            }}
+          >
+            Não Acertei 😞
+          </div>
+          <div
+            className="w-[200px] text-center bg-slate-200 p-2 text-slate-700 select-none cursor-pointer"
+            onClick={(e) => {
+              e.stopPropagation();
+              selecionarProximoCard();
+            }}
+          >
+            Acertei 😎
+          </div>
+        </div>
+      )}
     </div>
   );
 };
